2a7190dba577dab0cd3ed401a9c07b6f4d93b45b
Navin Prajapati <185079521+navin-prajapati-developer@users.noreply.github.com>
2026-06-07 09:59:18 +0530

    Add age classification based on user input

---
+++ conditions.py
@@ -0,0 +1,9 @@
+# If/Else conditions
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult!")
+elif age >= 13:
+    print("You are a teenager!")
+else:
+    print("You are a child!")
