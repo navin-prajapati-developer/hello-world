5778cc8ab02a2cfea4c5ace7c95d41b441f6257b
Navin Prajapati <185079521+navin-prajapati-developer@users.noreply.github.com>
2026-06-07 09:49:31 +0530

    Add basic user input program in Python

---
+++ basics.py
@@ -0,0 +1,8 @@
+# My first Python program
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print("Hello, my name is " + name)
+print("I am " + age + " years old")
+print("I live in " + city)
