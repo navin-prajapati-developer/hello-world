81c604e9e4f85637fa92cbb401dafd05c4d5e73b
Navin Prajapati <185079521+navin-prajapati-developer@users.noreply.github.com>
2026-06-07 09:55:51 +0530

    Add simple calculator functionality in calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Simple Calculator
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition: " + str(num1 + num2))
+print("Subtraction: " + str(num1 - num2))
+print("Multiplication: " + str(num1 * num2))
+print("Division: " + str(num1 / num2))
